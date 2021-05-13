8a2717ba78cea417944cc67872b9282e31fbdfff
namirinz <nathmcfc31115@gmail.com>
2021-05-13 14:32:33 +0700

    refactor code #4

--- src/core/visualize.py
+++ src/core/visualize.py
@@ -4,11 +4,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+BLUE_COLOR = [0, 0, 255]
+ORANGE_COLOR = [255, 140, 0]
+GREEN_COLOR = [0, 188, 0]
+
 def draw_bbox(
     bboxes: np.ndarray,
     image: np.ndarray,
-    format: str = 'xyxy'
-) -> np.ndarray :
+    format='xyxy',
+) -> np.ndarray:
     """
         Draw bounding box over image.
 
@@ -47,50 +51,58 @@ def draw_keypoint(
 
     image_plot = image.copy()
 
-    keypoint_pairs = [
-        [16, 14], [14, 12], [17, 15], [15, 13], 
-        [12, 13], [6, 12], [7, 13], [6, 7],
-        [6, 8], [7, 9], [8, 10], [9, 11],
-        [2, 3], [1, 2], [1, 3], [2, 4],
-        [3, 5], [4, 6], [5, 7]
+    keypoint_pairs = [[16, 14], [14, 12], [17, 15],
+        [15, 13], [12, 13], [6, 12], [7, 13],
+        [6, 7], [6, 8], [7, 9], [8, 10],
+        [9, 11], [2, 3], [1, 2], [1, 3],
+        [2, 4], [3, 5], [4, 6], [5, 7],
     ]
 
-    color_pairs = [
-        [0, 0, 255], [0, 0, 255], [0, 0, 255], [0, 0, 255], 
-        [0, 0, 255], [255, 140, 0], [255, 140, 0], [255, 140, 0], 
-        [255, 140, 0], [255, 140, 0], [255, 140, 0], [0, 128, 0],
-        [0, 128, 0], [0, 128, 0], [0, 128, 0], [0, 128, 0],
-        [0, 128, 0]
+    color_pairs = [BLUE_COLOR, BLUE_COLOR, BLUE_COLOR,
+        BLUE_COLOR, BLUE_COLOR, ORANGE_COLOR,
+        ORANGE_COLOR, ORANGE_COLOR, ORANGE_COLOR,
+        ORANGE_COLOR, ORANGE_COLOR, GREEN_COLOR,
+        GREEN_COLOR, GREEN_COLOR, GREEN_COLOR,
+        GREEN_COLOR, GREEN_COLOR,
     ]
 
     for pair in keypoint_pairs:
 
-        # tuple(keypoints[index])
-        first_point = tuple(keypoints[pair[0]-1].astype(np.int32))
-        second_point = tuple(keypoints[pair[1]-1].astype(np.int32))
-        
+        # keypoints [ index ]
+        first_point = keypoints[pair[0]-1].astype(np.int32)
+        first_point = tuple(first_point)
+
+        second_point = keypoints[pair[1]-1].astype(np.int32)
+        second_point = tuple(second_point)
+
         first_point_color = color_pairs[pair[0]-1]
         second_point_color = color_pairs[pair[1]-1]
         line_color = first_point_color
 
         # Plotting first point
-        image_plot = cv2.circle(img=image_plot, center=first_point, 
-                                color=first_point_color, radius=radius, thickness=-1)
-        
+        image_plot = cv2.circle(
+            img=image_plot, center=first_point,
+            color=first_point_color, radius=radius, thickness=-1
+        )
+
         # Plotting second point
-        image_plot = cv2.circle(img=image_plot, center=second_point, 
-                                color=second_point_color, radius=radius, thickness=-1)
+        image_plot = cv2.circle(
+            img=image_plot, center=second_point,
+            color=second_point_color, radius=radius, thickness=-1
+        )
 
         # Plotting line between 2 points
-        image_plot = cv2.line(img=image_plot, pt1=first_point, pt2=second_point, 
-                              color=line_color, thickness=thickness)
-    
+        image_plot = cv2.line(
+            img=image_plot, pt1=first_point, pt2=second_point,
+            color=line_color, thickness=thickness
+        )
+
     if show:
         plt.imshow(cv2.cvtColor(image_plot, cv2.COLOR_BGR2RGB))
         plt.axis('off')
         plt.show()
-    
+
     if save_path is not None:
         cv2.imwrite(save_path, image_plot)
-    
+
     return image_plot
