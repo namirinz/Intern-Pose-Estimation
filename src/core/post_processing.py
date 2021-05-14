7c5a4ef65e48b99a33aa9378d2f65a2b22929a88
namirinz <nathmcfc31115@gmail.com>
2021-05-14 08:48:23 +0700

    refactor #1

--- src/core/post_processing.py
+++ src/core/post_processing.py
@@ -1,5 +1,4 @@
 """post-processing module after inference data."""
-import cv2
 import numpy as np
 
 from typing import List, Dict
@@ -11,24 +10,24 @@ IMAGE_WIDTH = 192
 def process_yolo(
         yolo_bboxes: np.ndarray,
         bbox_thr: float,
-        class_id = 0,
+        class_id=0,
     ) -> List[Dict[str, np.ndarray]]:
     """Post processing bounding box output from yolo to mmpose format.
 
     Args:
-        bounding_boxes (np.ndarray): [N, 6] Output bounding boxes from yolo.
+        yolo_bboxes (np.ndarray): [N, 6] Output bounding boxes from yolo.
             [xmin, ymin, xmax, ymax, score, class_id]
         bbox_thr (float): bounding box threshold to be keep.
         class_id (int): Class id to select 0 default for person object.
 
     Returns:
         List N length contain dictionary with 'bbox' key and
-            bouning box as value. 
+            bouning box as value.
     """
     bboxes_all = yolo_bboxes.xyxy[0].detach().cpu().numpy()
     bboxes_person = bboxes_all[bboxes_all[:, 5] == class_id]
     bboxes_select = bboxes_person[bboxes_person[:, 4] > bbox_thr]
-    bboxes_select = bboxes_select[:, :5] # unselect class id at index 5
+    bboxes_select = bboxes_select[:, :5]  # unselect class id at index 5
     person_results = []
 
     for bbox in bboxes_select:
@@ -44,6 +43,7 @@ def resize_keypoint(
         bounding_boxes: np.ndarray,
     ) -> np.ndarray:
     """Resizing keypoint in range of 256 (Height), 192 (Width).
+    
     1. Cap bounding box and keypoint in an X, Y axis.
     2. find ratio to resize on X and Y.
     3. resize keypoint by computed ratio.
@@ -56,22 +56,22 @@ def resize_keypoint(
     Returns:
         List of resized keypoint [N, 17, 2].
     """
-    ### 1 ###
+    xmin, ymin, xmax, ymax = bounding_boxes[:4]
+    # 1 #
     keypoints_copy = keypoints.copy()
     keypoints_copy_X = keypoints_copy[:, 0] - xmin
     keypoints_copy_Y = keypoints_copy[:, 0] - ymin
 
-    ### 2 ###
-    xmin, ymin, xmax, ymax = bounding_boxes[:4]
-    ratioX = (xmax - xmin) * IMAGE_WIDTH
-    ratioY = (ymax - ymin) * IMAGE_HEIGHT
+    # 2 #
+    ratio_x = (xmax - xmin) * IMAGE_WIDTH
+    ratio_y = (ymax - ymin) * IMAGE_HEIGHT
 
-    ### 3 ###
-    keypoints_copy_X_resize = keypoints_copy_X * ratioX
-    keypoints_copy_Y_resize = keypoints_copy_Y * ratioY
+    # 3 #
+    keypoints_copy_x_resize = keypoints_copy_X * ratio_x
+    keypoints_copy_y_resize = keypoints_copy_Y * ratio_y
 
     keypoints_resize = np.concatenate(
-        [keypoints_copy_X_resize, keypoints_copy_Y_resize], axis=1
+        [keypoints_copy_x_resize, keypoints_copy_y_resize], axis=1,
     )
 
     return keypoints_resize
